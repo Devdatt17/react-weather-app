a706f2fe2f9a49c9862ad22276f79332c32fd5c3
Devdatt17 <devdattpandit99@gmail.com>
2022-03-11 11:15:08 +0530

    added error message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import {SEARCH_TEXT} from './redux/action'
+import { SEARCH_TEXT } from './redux/action'
 import { WEATHER_DATA } from './redux/action';
 import './App.css';
 
@@ -8,19 +8,19 @@ import './App.css';
 function App() {
 
   const dispatch = useDispatch()
-  const searchText = useSelector((state)=>state.reducer)
-  const weatherData = useSelector((state)=>state.weatherReducer)
-    const search = evt =>{
-      if (evt.key === "Enter"){
-        dispatch(WEATHER_DATA(searchText))
-        dispatch(SEARCH_TEXT(''))
-      }
+  const searchText = useSelector((state) => state.reducer)
+  const weatherData = useSelector((state) => state.weatherReducer)
+  const search = evt => {
+    if (evt.key === "Enter") {
+      dispatch(WEATHER_DATA(searchText))
+      dispatch(SEARCH_TEXT(''))
     }
+  }
 
-  const dateBuilder = d =>{
+  const dateBuilder = d => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
+
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
@@ -30,30 +30,30 @@ function App() {
   }
 
   return (
-      <div>
-        <div className={
-            (typeof weatherData.main != "undefined")
-            ? ((weatherData.main.temp > 16) 
-              ? 'App warm'
-                :'App')
-                :'App'}>
-            
-            <main>
-              <div className="search-box">
-                <input
-                type="text"
-                className="search-bar"
-                placeholder="Search..."
-                onChange={e=>dispatch(SEARCH_TEXT(e.target.value))}
-                value={searchText}
-                onKeyPress={search}
-                />
-              </div>
-              {(typeof weatherData.main != "undefined") ? (
-              <div>
-                <div className="location-box">
-                <div  className="location">{weatherData.name}, {weatherData.sys.country}</div>
-                  <div className="date">{dateBuilder(new Date())}</div>
+    <div>
+      <div className={
+        (typeof weatherData.main != "undefined")
+          ? ((weatherData.main.temp > 16)
+            ? 'App warm'
+            : 'App')
+          : 'App'}>
+
+        <main>
+          <div className="search-box">
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange={e => dispatch(SEARCH_TEXT(e.target.value))}
+              value={searchText}
+              onKeyPress={search}
+            />
+          </div>
+          {weatherData && weatherData.cod === 200 ? (
+            <div>
+              <div className="location-box">
+                <div className="location">{weatherData.name}, {weatherData.sys.country}</div>
+                <div className="date">{dateBuilder(new Date())}</div>
               </div>
               <div className="weather-box">
                 <div className="temp">
@@ -61,12 +61,22 @@ function App() {
                 </div>
                 <div className="weather">{weatherData.weather[0].description}</div>
               </div>
+            </div>
+          ) : weatherData.message ?
+            (
+              <div className="location-box">
+                <div className='location'>
+                  <h2 className="error-message">
+                    {weatherData.message}
+                  </h2>
+                </div>
               </div>
-              ):('')}
-            </main>
-          </div>
-      </div>  
-    );
+            )
+            : ''}
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
